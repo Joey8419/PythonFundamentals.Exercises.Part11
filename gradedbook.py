86a5e7bb30a9af3e0d1c12ecae5a4c6d52deebc2
Joey8419 <jnelson297@gmail.com>
2023-11-14 01:03:16 -0500

    Gotta make the test

--- gradedbook.py
+++ gradedbook.py
@@ -26,3 +26,59 @@ class Person:
 
     def update_alive_status(self, alive_status):
         self.alive_status = alive_status
+
+        # Declare instructor class and class must inherit from Person class above
+        class Instructor(Person):
+
+            def __init__(self, first_name, last_name, dob, alive_status):
+                # Call __init__ method of base class (Person)
+                super().__init__(first_name, last_name, dob, alive_status)
+
+                # Generate Universal Unique Identifier for instructor id
+                self.instructor_id = f"Instructor Universal Unique Identifier (uuid)"
+
+        class Student(Person):
+
+            def __init__(self, first_name, last_name, dob, alive_status):
+                # Call __init__ method of base class (Person)
+                super().__init__(first_name, last_name, dob, alive_status)
+
+            # Generate Universal Unique Identifier for instructor id
+            self.instructor_id = f"Student Universal Unique Identifier (uuid)"
+
+        class ZipCodeStudent(Student):
+            # If 'ZipCodeStudent'  does not have any additional attributes or methods beyond what is has inherited
+            # from the 'Student' class, then the method body remains empty:
+            pass
+
+        class CollegeStudent(Student):
+            # If 'CollegeStudent'  does not have any additional attributes or methods beyond what is has inherited
+            # from the 'Student' class, then the method body remains empty:
+            pass
+
+        class Classroom:
+
+            def __init__(self):
+                self.students = []
+                self.instructors = []
+
+            def add_student(self, student):
+                self.students.append(student)
+
+            def add_instructor(self, instructor):
+                self.instructors.append(instructor)
+
+            def remove_student(self, student):
+                self.students.remove(student)
+
+            def remove_instructor(self, instructor):
+                self.instructors.remove(instructor)
+
+            def print_students(self):
+                for student in self.students:
+                    print(f"Student: {student.first_name} {student.last_name}, ID: {Student.student_id}")
+
+            def print_instructors(self):
+                for instructor in self.instructors:
+                    print(
+                        f"Instructors: {instructor.first_name} {instructor.last_name}, ID: {Instructor.instructor_id}")
